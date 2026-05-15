0d0517951ecbf4e835625866639dae809045cfc5
Tadros Eliott <53917558+Telmo26@users.noreply.github.com>
2026-05-15 16:34:40 +0200

    Added trustmap parsing using Serde

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,33 @@ mod lexer;
 mod parser;
 mod type_checker;
 mod interpreter;
+mod trustmap;
 
 use crate::{
-    interpreter::Interpreter, lexer::Token, parser::parse, type_checker::type_check
+    interpreter::Interpreter, lexer::Token, parser::parse, trustmap::TrustMap, type_checker::type_check
 };
 
 #[derive(Parser, Debug)]
 struct Args {
     file: PathBuf,
+
+    #[arg(long, name="trustmap.json")]
+    trustmap: Option<PathBuf>
 }
 
-fn main() -> std::io::Result<()> {
+fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let file_text = std::fs::read_to_string(args.file)?;
+    let trustmap = if let Some(tm_path) = args.trustmap {
+        let json = std::fs::read_to_string(tm_path)?;
+        Some(TrustMap::from_json(&json)?)
+    } else {
+        None
+    };
+
+    if let Some(tm) = trustmap.as_ref() {
+        dbg!(tm);
+    }
 
     let lexer = Token::lexer(&file_text);
     let ast = parse(lexer).unwrap();
